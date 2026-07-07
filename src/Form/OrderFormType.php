a5b65a39c90b770ceeab6ebe0b000ab227069165
Aditigro <52889510+Aditigro@users.noreply.github.com>
2026-07-07 10:47:22 +0300

    Restrict order packages to available packages

--- src/Form/OrderFormType.php
+++ src/Form/OrderFormType.php
@@ -9,6 +9,7 @@ use App\Entity\Order;
 use App\Entity\Package;
 use App\Repository\OrderRepository;
 use App\Repository\PackageRepository;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
@@ -27,6 +28,12 @@ class OrderFormType extends AbstractType
             ->add('package', EntityType::class, [
                 'class' => Package::class,
                 'choice_label' => 'name',
+                'query_builder' => function (PackageRepository $repository): QueryBuilder
+                {
+                    return $repository->createQueryBuilder('p')
+                        ->leftJoin('p.consumer_order', 'o')
+                        ->where('o.id IS NULL');
+                }
             ])
             ->add('submit', SubmitType::class);
 
