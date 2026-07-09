c0849a3b37cf6277f17cca168487351943517636
Aditigro <52889510+Aditigro@users.noreply.github.com>
2026-07-09 12:13:35 +0300

    Dynamically update navbar to show only available links

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        if($this->isGranted("ROLE_CONSUMER"))
+        {
+            return $this->redirectToRoute('app_consumer');
+        }
+        if($this->isGranted("ROLE_BUSINESS"))
+        {
+            return $this->redirectToRoute('app_business');
+        }
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/denied', name: 'app_access_denied')]
+    public function accessDenied() : Response
+    {
+        return $this->render('security/denied.html.twig');
+    }
+}
