5efd75ccb9c9427f5dcf8f4c1852c0b694fafb61
Aditigro <52889510+Aditigro@users.noreply.github.com>
2026-07-14 17:47:36 +0300

    Add mailing capability

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -12,14 +12,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
+    public function register(MailerInterface $mailer, UrlGeneratorInterface $urlGenerator , Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
         $consumer= new Consumer();
@@ -40,7 +44,21 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // do anything else you need here, like send an email
+            $loginUrl = $urlGenerator->generate('app_login', [], UrlGeneratorInterface::ABSOLUTE_URL);
+            $email = (new Email())
+                ->from('ecomeal@example.com')
+                ->to($user->getEmail())
+                ->subject('Successful Registration')
+                ->html(sprintf('<p>Welcome to our application. Link to <a href="%s">log in</a></p>', $loginUrl));
+
+            // puteti folosi si template-uri pentru mesaje.
+            //            ->htmlTemplate('emails/welcome.html.twig')
+            //            ->context([
+            //                'userName' => $userName,
+            //                'loginUrl' => $loginUrl,
+            //            ]);
+
+            $mailer->send($email);
 
             $security->login($user, 'App\Security\LoginFormAuthenticator', 'main');
             return $this->redirectToRoute('app_consumer');
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(MailerInterface $mailer, Security $security,AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
